26ef00988057a88c3da6851f676d67ac941bce80
leandro vicente <leandro.vicente98@outlook.com>
2022-05-20 21:16:50 -0300

    feat: install docker and config sequelize-mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TransactionModule } from './transaction/transaction.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Transaction } from 'sequelize/types';
 
 @Module({
-  imports: [TransactionModule],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: process.env.DB_CONNECTION as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      models: [Transaction],
+      autoLoadModels: true,
+      synchronize: true,
+      sync: {
+        alter: true,
+      },
+    }),
+    TransactionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
